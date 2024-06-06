09c4e174681dfffeea3b5752fa70c8bcd38137b2
Arthur Scaratti <scarattiarthur@gmail.com>
2024-06-05 21:36:26 -0300

    up

--- src/app/components/toolbar/side-nav/side-nav.component.ts
+++ src/app/components/toolbar/side-nav/side-nav.component.ts
@@ -1,30 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
 import { RouterLink, ActivatedRoute } from '@angular/router';
 import { BreadcrumbComponent } from '../../staples/breadcrumb/breadcrumb.component';
 import { ApicallService } from '../../../services/apicall.service';
 import { HttpClient } from '@angular/common/http';
 import { ListChapters } from '../../../models/listchapters';
-import { NgClass } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [RouterLink, BreadcrumbComponent, NgClass],
+  imports: [RouterLink, BreadcrumbComponent, NgClass, NgFor],
   providers: [ApicallService, HttpClient],
   templateUrl: './side-nav.component.html',
-  styleUrl: './side-nav.component.scss',
+  styleUrls: ['./side-nav.component.scss'],
 })
-export class SideNavComponent {
+export class SideNavComponent implements AfterViewInit {
   chapters?: ListChapters[] = [];
   novelName?: string = '';
   urlName: any;
   language: any;
-  capNro: number = 0;
+  capNro = this.route.snapshot.paramMap.get('capNro') ?? '';
+
+  @ViewChildren('chapterItem') chapterItems!: QueryList<ElementRef>;
+
   constructor(
     private apicallservice: ApicallService,
     private route: ActivatedRoute,
   ) {
     this.obterCapitulos();
+  }
+
+  ngAfterViewInit() {
+    this.scrollToCurrentChapter();
   }
 
   obterCapitulos() {
@@ -33,12 +40,21 @@ export class SideNavComponent {
       if (chapters && chapters.chapters) {
         this.chapters = chapters.chapters.filter(chapter => chapter.language === 'EN');
         this.novelName = chapters.novelName;
+        this.scrollToCurrentChapter();
       }
     });
   }
 
+  scrollToCurrentChapter() {
+    const currentChapterElement = this.chapterItems.find(
+      item => item.nativeElement.dataset.capnro == parseInt(this.capNro) -1
+    );
+    if (currentChapterElement) {
+      currentChapterElement.nativeElement.scrollIntoView({ behavior: 'auto', block: 'start' });
+    }
+  }
+
   gerarHref(capNro: number): string {
-    // Suponha que você tenha uma rota base
     const rotaBase = `/home/${this.urlName}/chapters/${capNro}`;
     return rotaBase;
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://58500b64e7288e7ef15c68ba512b7ac4.serveo.net',
+  apiUrl: 'http://localhost:5047',
 };
