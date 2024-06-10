46a939795659a9dd5d2e1a308bf1b186f578b758
Arthur Scaratti <scarattiarthur@gmail.com>
2024-06-10 10:45:25 -0300

    up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://8c01431188c1a47f69492f723b27ab8d.serveo.net', //up
+  apiUrl: 'https://d0430a81744d19cec8ca6be5fc7c305f.serveo.net', //up
 };
